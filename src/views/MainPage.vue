405dda3c5ffbc28a893f0c7b400ebe718cd5732b
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-24 23:11:48 +0900

    add:MainPageForm.vue, MainPagevue,NotFoundPage.vue

---
+++ src/views/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <MainPageForm></MainPageForm>
+  </div>
+</template>
+
+<script>
+import MainPageForm from "../components/posts/MainPageForm.vue";
+export default {
+  components: {
+    MainPageForm,
+  },
+};
+</script>
+
+<style></style>
