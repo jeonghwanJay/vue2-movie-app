a63d8119544b56a4880c14f02f6d9ce60a767f4f
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-25 22:25:19 +0900

    edit : index.html, AppHeader.vue

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,19 +1,19 @@
 <template>
   <header class="appHeader">
-    <a href="/" class="banner">OMDbAPI.COM</a>
+    <a href="/" class="banner">OMDbAPI<span class="dotcom">.COM</span></a>
     <div class="nav-container">
-      <div class="nav-item">
-        <a href="/">Search</a>
+      <div>
+        <a class="nav-item" href="/">Search</a>
       </div>
-      <div class="nav-item">
-        <a href="/movie">Movie</a>
+      <div>
+        <a class="nav-item" href="/movie">Movie</a>
       </div>
-      <div class="nav-item">
-        <a href="/about">About</a>
+      <div>
+        <a class="nav-item" href="/about">About</a>
       </div>
     </div>
     <div class="profile">
-      <img src="../../assets/MyProfile.jpeg" alt="MyProfile" align="right" />
+      <img src="../../assets/MyProfile.jpeg" alt="MyProfile" />
     </div>
   </header>
 </template>
@@ -24,25 +24,41 @@ export default {};
 
 <style scoped>
 .appHeader {
-  padding: 0;
-  margin: 1rem 1rem 2rem 1rem;
+  height: 4.5rem;
   display: flex;
+  align-items: center;
+  padding: 0 2.4rem;
+  position: relative;
+  font-family: "Oswald", sans-serif;
 }
 .banner {
+  text-decoration: none;
+  font-size: 1.4rem;
   color: #fdc000;
-  font-size: 1.2rem;
+  margin: 0 2.3rem 0 0;
+}
+.dotcom {
   font-weight: bold;
+  text-decoration: none;
+  color: black;
 }
 .nav-container {
+  text-decoration: none;
   display: flex;
 }
 .nav-item {
+  text-decoration: none;
+  padding: 0.8rem;
+  font-family: "Roboto", sans-serif;
+  color: #fdc000;
 }
 .profile {
-  display: inline;
+  position: absolute;
+  right: 3rem;
+  top: 1.4rem;
 }
 .profile img {
-  width: 1.6rem;
-  border-radius: 1.2rem;
+  width: 1.8rem;
+  border-radius: 1rem;
 }
 </style>
