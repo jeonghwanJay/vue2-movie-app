7cf0a04277a88cbf602ba9277ca7b566f2d9688c
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-27 23:16:15 +0900

    edit : MoviePageForm.vue

--- src/components/posts/MoviePageForm.vue
+++ src/components/posts/MoviePageForm.vue
@@ -1,19 +1,56 @@
 <template>
-  <div>
-    <img src="../../assets/ironman3.png" alt="ironman3" />
-    <h1>Iron Man3</h1>
-    <p>03 May 2013130 minUnited States</p>
-    <p>
-      Marvel's "Iron Man 3" pits brash-but-brilliant industrialist Tony
-      Stark/Iron Man against an enemy whose reach knows no bounds. When Stark
-      finds his personal world destroyed at his enemy's hands, he embarks on a
-      harrowing quest to find those responsible. This journey, at every turn,
-      will test his mettle. With his back against the wall, Stark is left to
-      survive by his own devices, relying on his ingenuity and instincts to
-      protect those closest to him. As he fights his way back, Stark discovers
-      the answer to the question that has secretly haunted him: does the man
-      make the suit or does the suit make the man?
-    </p>
+  <div class="movie-detail">
+    <div>
+      <img src="../../assets/ironman3.png" alt="ironman3" />
+    </div>
+    <div>
+      <h1>Iron Man3</h1>
+      <span>03 May 2013130 minUnited States</span>
+      <div>
+        Marvel's "Iron Man 3" pits brash-but-brilliant industrialist Tony
+        Stark/Iron Man against an enemy whose reach knows no bounds. When Stark
+        finds his personal world destroyed at his enemy's hands, he embarks on a
+        harrowing quest to find those responsible. This journey, at every turn,
+        will test his mettle. With his back against the wall, Stark is left to
+        survive by his own devices, relying on his ingenuity and instincts to
+        protect those closest to him. As he fights his way back, Stark discovers
+        the answer to the question that has secretly haunted him: does the man
+        make the suit or does the suit make the man?
+      </div>
+    </div>
+    <div>
+      <h1>Ratings</h1>
+      <div>
+        <div>
+          <img src="../../assets/IMDB.png" alt="IMDB" />
+          <span>7.1/10</span>
+        </div>
+        <div>
+          <img src="../../assets/Rotten Tomatoes.png" alt="RT" />
+          <span>79%</span>
+        </div>
+        <div>
+          <img src="../../assets/Metacritic.png" alt="MC" />
+          <span>62/100</span>
+        </div>
+      </div>
+      <div>
+        <h1>Actors</h1>
+        <span>Robert Downey Jr., Guy Pearce, Gwyneth Paltrow</span>
+      </div>
+      <div>
+        <h1>Director</h1>
+        <span>Shane Black</span>
+      </div>
+      <div>
+        <h1>Production</h1>
+        <span>N/A</span>
+      </div>
+      <div>
+        <h1>Genre</h1>
+        <span>Action, Adventure, Sci-Fi</span>
+      </div>
+    </div>
   </div>
 </template>
 
